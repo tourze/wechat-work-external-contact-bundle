6b615ca75472e17342e66d60f12bf52c5378026a
tourze <tourze@users.noreply.github.com>
2025-06-05 01:45:39 +0800

    新增 `loadByUnionIdAndCorp` 方法以实现 `ExternalUserLoaderInterface` 接口功能

--- src/Repository/ExternalUserRepository.php
+++ src/Repository/ExternalUserRepository.php
@@ -4,6 +4,10 @@ namespace WechatWorkExternalContactBundle\Repository;
 
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\DependencyInjection\Attribute\AsAlias;
+use Tourze\WechatWorkContracts\CorpInterface;
+use Tourze\WechatWorkExternalContactModel\ExternalContactInterface;
+use Tourze\WechatWorkExternalContactModel\ExternalUserLoaderInterface;
 use WechatWorkExternalContactBundle\Entity\ExternalUser;
 
 /**
@@ -12,10 +16,19 @@ use WechatWorkExternalContactBundle\Entity\ExternalUser;
  * @method ExternalUser[]    findAll()
  * @method ExternalUser[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
-class ExternalUserRepository extends ServiceEntityRepository
+#[AsAlias(ExternalUserLoaderInterface::class)]
+class ExternalUserRepository extends ServiceEntityRepository implements ExternalUserLoaderInterface
 {
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, ExternalUser::class);
+    }
+
+    public function loadByUnionIdAndCorp(string $unionId, CorpInterface $corp): ?ExternalContactInterface
+    {
+        return $this->findOneBy([
+            'unionId' => $unionId,
+            'corp' => $corp,
+        ]);
     }
 }
